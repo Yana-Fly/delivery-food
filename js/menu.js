47e39836f83e0847e993705da31d6aa541345f4b
Yana-Fly <83606127+Yana-Fly@users.noreply.github.com>
2021-11-06 12:25:51 +0300

    files changed in lesson 2

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+const restaurant = 'food-band';
+
+const renderItems = (data) => {
+    for (let i = 0; i < data.length; i++) {
+        console.log(data[i]);
+    }
+}
+
+fetch(`./db/${restaurant}.json`)
+    .then((response) => response.json())
+    .then((data) => {
+        renderItems(data);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
